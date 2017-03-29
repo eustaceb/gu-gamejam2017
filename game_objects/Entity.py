0a0c29f4f8263a77147a7897986735625b2d80c2
KinRyuuXIV <tom.f.whitehouse@btinternet.com>
2017-03-29 23:25:54 +0100

    Basic player class

--- game_objects/Entity.py
+++ game_objects/Entity.py
@@ -1,5 +1,3 @@
-import pygame
-from pygame.rect import Rect
 from pygame.sprite import Sprite
 
 #This class represents every entity in the game
@@ -15,14 +13,19 @@ class Entity(Sprite):
     x_velocity = 0
     y_velocity = 0
 
+    base_acceleration = 5
+    max_velocity = 10
+
     # Other properties
     gravity = 1
+    jump_velocity = 5
+    jumping = False
     falling = False
     solid = True
     visible = True
 
     def __init__(self, rect=None, image=None, x_velocity=0, y_velocity=0, gravity=1, falling=False,
-                 solid=True, visible=True):
+                 jumping=False, solid=True, visible=True):
         super(Entity, self).__init__()
 
         self.rect = rect
@@ -32,6 +35,7 @@ class Entity(Sprite):
         self.y_velocity = y_velocity
 
         self.gravity = gravity
+        self.jumping = jumping
         self.falling = falling
         self.solid = solid
         self.visible = visible
@@ -40,8 +44,15 @@ class Entity(Sprite):
         if self.falling and (self.gravity != 0 and self.gravity is not None):
             self.yvel += self.gravity
 
+        self.
+
         #Generic Collision code or something
 
     def render(self, screen):
         if self.image and self.rect and self.visible is True:
             self.screen.blit(self.image, self.rect);
+
+    def jump(self):
+        if self.jumping is False and self.falling is False:
+            self.jumping = True
+            self.y_velocity = -self.jump_velocity
