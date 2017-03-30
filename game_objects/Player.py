d9058cca60fd8b39e3ffe5c3fc2cd1de2764edb5
KinRyuuXIV <tom.f.whitehouse@btinternet.com>
2017-03-30 02:19:00 +0100

    slow code

--- game_objects/Player.py
+++ game_objects/Player.py
@@ -1,4 +1,4 @@
-from . import Entity
+from .Entity import Entity
 import pygame
 import math
 
@@ -10,21 +10,27 @@ class Player(Entity):
     key_left = pygame.K_A
     key_right = pygame.K_D
 
+    def __init__(self):
+        super(Player, self).__init__()
+
     def update(self):
 
         key = pygame.key.get_pressed()
 
-        if key[self.key_up]:
-            if not key[self.key_down]:
-                self.move_up()
-        elif key[self.key_down]:
-            self.move_down()
+        if key[self.key_up] and key[self.key_down] and key[self.key_left] and key[self.key_right]:
+            self.slow()
+        else:
+            if key[self.key_up]:
+                if not key[self.key_down]:
+                    self.move_up()
+            elif key[self.key_down]:
+                self.move_down()
 
-        if key[self.key_left]:
-            if not key[self.key_right]:
-                self.move_left()
-        elif key[self.key_right]:
-            self.move_right()
+            if key[self.key_left]:
+                if not key[self.key_right]:
+                    self.move_left()
+            elif key[self.key_right]:
+                self.move_right()
 
         velocity = math.sqrt(math.pow(self.x_velocity, 2) + math.pow(self.y_velocity, 2))
 
