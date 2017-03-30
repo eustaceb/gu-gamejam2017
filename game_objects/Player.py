2f75f50df00041ad061a676b93ced67e9f4069d3
KinRyuuXIV <tom.f.whitehouse@btinternet.com>
2017-03-30 19:02:00 +0100

    Added a minus sign

--- game_objects/Player.py
+++ game_objects/Player.py
@@ -8,19 +8,23 @@ import pygame
 class Player(PhysicsEntity):
 
     tractor_beam = None
+
+    rotation_angle = 0
+    max_rotation = 15
+
     key_up = pygame.K_w
     key_down = pygame.K_s
     key_left = pygame.K_a
     key_right = pygame.K_d
+    original_image = None
     
     def __init__(self, **kwargs):
         self.tractor_beam = TractorBeam(rect=Rect(0,0,64,64), player=self)
-
         super(Player, self).__init__(**kwargs)
+        self.original_image = self.image
 
     def update(self, tilemap, entities):
 
-        self.slow()
         key = pygame.key.get_pressed()
 
         if key[self.key_up]:
@@ -28,14 +32,24 @@ class Player(PhysicsEntity):
                 self.move_up()
         elif key[self.key_down]:
             self.move_down()
+        else:
+            self.slowY()
 
         if key[self.key_left]:
             if not key[self.key_right]:
                 self.move_left()
         elif key[self.key_right]:
             self.move_right()
+        else:
+            self.slowX()
 
         super(Player, self).update(tilemap, entities)
+
+        self.rotation_angle = self.x_velocity/self.max_velocity * self.max_rotation
+
+        if self.x_velocity != 0:
+            self.image = pygame.transform.rotate(self.original_image, -self.rotation_angle)
+
         self.tractor_beam.update()
 
 
