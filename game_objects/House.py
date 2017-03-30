b4962cc324702a292d25c9fdc55ad7c34eb1f0e6
KinRyuuXIV <tom.f.whitehouse@btinternet.com>
2017-03-30 23:08:38 +0100

    more sprites and house fix

--- game_objects/House.py
+++ game_objects/House.py
@@ -1,21 +1,21 @@
 import pygame
-from .PhysicsEntity import PhysicsEntity
+from .Entity import Entity
 
 
-class House(PhysicsEntity):
+class House(Entity):
     def __init__(self, rect, image, player=None, **kwargs):
         self.images = []
         self.health = 50
         self.player = player
         self.destroyed = False
         self.tractor = pygame.sprite.Group(player.tractor_beam)
-        super(House, self).__init__(rect, image, gravity=0, **kwargs)
+        super(House, self).__init__(rect, image, **kwargs)
 
     def set_images(self, images):
         self.images = images
 
     def update(self, tilemap=None, entities=None, **kwargs):
-        super(House, self).update(tilemap, entities)
+        super(House, self).update()
 
     def handle_collisions(self, tilemap):
         super(House, self).handle_collisions(tilemap)
