7a92cfdbb5301c2ff33ec07ef1f88317ea8bca01
eustaceb <Justas.B94@gmail.com>
2017-03-31 14:32:05 +0100

    Menu shows score

--- Menu.py
+++ Menu.py
@@ -2,7 +2,7 @@ import pygame
 
 
 class Menu:
-    def __init__(self, screen):
+    def __init__(self, screen, score):
         self.restart = False
         self.font_size = 64
         self.char_w = 36
@@ -11,12 +11,15 @@ class Menu:
         self.draw_surface = pygame.Surface((screen.get_width(), screen.get_height()), pygame.SRCALPHA)
 
         self.gover_str = "GAME OVER"
-        self.restart_str = "RESTART"
-        self.quit_str = "QUIT"
+        self.score_str = "SCORE: " + str(score)
+        self.restart_str = "RESTART (r)"
+        self.quit_str = "QUIT (q)"
 
         center = (screen.get_width() / 2, screen.get_height() / 2)
         self.gover_text = self.font.render(self.gover_str, 1, (255, 255, 50))
-        self.gover_pos = (center[0] - (self.char_w * len(self.gover_str))/2, center[1] - 3*self.font_size)
+        self.gover_pos = (center[0] - (self.char_w * len(self.gover_str))/2, center[1] - 4*self.font_size)
+        self.score_text = self.font.render(self.score_str, 1, (255, 255, 50))
+        self.score_pos = (center[0] - (self.char_w * len(self.score_str))/2, center[1] - 3*self.font_size)
         self.restart_text = self.font.render(self.restart_str, 1, (50, 255, 50))
         self.restart_pos = (center[0] - (self.char_w * len(self.restart_str))/2, center[1] - self.font_size)
         self.quit_text = self.font.render(self.quit_str, 1, (255, 50, 50))
@@ -33,6 +36,7 @@ class Menu:
         self.draw_surface.fill((0, 0, 0, 1))
         screen.blit(self.draw_surface, (0, 0))
         screen.blit(self.gover_text, self.gover_pos)
+        screen.blit(self.score_text, self.score_pos)
         screen.blit(self.restart_text, self.restart_pos)
         screen.blit(self.quit_text, self.quit_pos)
 
@@ -60,9 +64,9 @@ class Menu:
     def process_event(self, event):
         # Key events
         if (event.type == pygame.KEYDOWN and event.key == pygame.K_q) or event.type == pygame.QUIT:
-            return "quit"
+            return -1
         if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
-            return "restart"
+            return 1
         # Mouse events
         if event.type == pygame.MOUSEBUTTONDOWN:
             if self.restart_rect.collidepoint(event.pos[0], event.pos[1]):
